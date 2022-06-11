dbc2c9bc00825ec9fd28a5da999291b12f8c023c
castlemin999 <castlemin999@gmail.com>
2022-06-11 15:45:08 +0900

    add AppConfig

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,14 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // MemberServiceImpl이 구현체를 선택해야하는 책임을 덜기 위해 생성자를 이용해 외부에서 구현체를 설정.
+    // 생성자 주입
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,8 +5,21 @@ import hello.core.member.*;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private DiscountPolicy discountPolicy; // DIP를 지키기 위해 누군가가 구현체를 주입해줘야함 (FixDiscountPolicy or RateDiscountPolicy)
+    /*
+     * DIP 원칙
+     * 고차원 모듈은 저차원 모듈에 의존하면 안된다.
+     * OrderServiceImpl은 DiscountPolicy에만 의존해야 DIP가 성립되는데
+     * FixDiscountPolicy or RateDiscountPolicy 둘에도 의존하기 떄문에 DIP에 위반한다.
+     * 고로 DIP를 지키기 위해 외부에서 구현체를 주입해줘야함.
+     * */
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
