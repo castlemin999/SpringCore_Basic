2e7e6fe4bd732c018b12066cc32dd772f96c199c
castlemin999 <castlemin999@gmail.com>
2022-06-12 15:25:21 +0900

    AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,6 +12,9 @@ import hello.core.order.OrderServiceImpl;
 
 // 공연 기획자 역할
 public class AppConfig {
+
+    /*
+    역할과 구현이 명확하게 나누어져 있지 않다.
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
@@ -19,5 +24,28 @@ public class AppConfig {
                 new MemoryMemberRepository(),
                 new RateDiscountPolicy()
         );
+    }
+    */
+
+    // 역할
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // 구현
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // 역할
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // 구현
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+
     }
 }
