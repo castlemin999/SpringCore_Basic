d9863bb0825239585ea5babd919ecaff91c59b00
castlemin999 <castlemin999@gmail.com>
2022-06-21 21:44:36 +0900

    make Annotation & Use Beans by List, Map

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy // 수동으로 생성한 annotation
-//@Primary // 동일한 타입의 Bean이 있을 경우 우선
+// @Qualifier("mainDiscountPolicy") String은 컴파일 단계에서 오타를 잡아 낼 수 없음.
+@MainDiscountPolicy // 수동으로 생성한 annotation 사용하면 해결 됨
+// @Primary // 동일한 타입의 Bean이 있을 경우 우선
 // @Autowired는 동일한 타입의 Bean이 있을 경우 필드명, 파라미터명을 확인하고 해당 변수명과 같은 클래스를 주입한다.
-// @Qualifier("mainDiscountPolicy")
 // Qualifier와 Primary 둘다 있을때는 Qualifier가 우선
 public class RateDiscountPolicy implements DiscountPolicy{
 
